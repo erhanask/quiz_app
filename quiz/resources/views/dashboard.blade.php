1d8e279f528782f040c2b0e29edda4259e477d94
erhanask <Canimbanam28>
2022-01-24 21:56:26 +0300

    Jetstream installed ! Layout setted .

---
+++ quiz/resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot name="header">
+        Anasayfa
+    </x-slot>
+
+    <x-slot name="slot">
+    <div class="alert alert-success">
+        Bootstrap working
+    </div>
+
+    </x-slot>
+</x-app-layout>
